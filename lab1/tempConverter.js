5ec52ae092b45b631d9b6eddad3830e95e6afa2d
LyCoffy <lily_anacoffy@hotmail.com>
2025-09-23 15:54:09 -0700

    Temperature Convertedd homework Week 1

---
+++ lab1/tempConverter.js
@@ -0,0 +1,7 @@
+let fahrenheit = 65 // Average temperature in Monterrey, Mexico for November 2024
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log("Temperature (fahrenheit): " + fahrenheit)
+console.log("Temperature (celsius): " + celsius)
+console.log("Temperature (kelvin): " + kelvin)
